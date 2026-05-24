618fe73e9469ea6fcfce212489cef73c004c5e3d
Lucien Barnes <Lucienpb@users.noreply.huggingface.co>
2026-05-24 15:56:00 +0000

    Update utils/github_storage.py

--- utils/github_storage.py
+++ utils/github_storage.py
@@ -43,6 +43,8 @@ def github_load_json(path):
     Returns Python dict/list or None if file does not exist.
     """
     url = f"https://api.github.com/repos/{GITHUB_REPO}/contents/{path}?ref={GITHUB_BRANCH}"
+    print("DEBUG URL:", url)
+    print("DEBUG REPO:", GITHUB_REPO)
 
     try:
         data = _github_request("GET", url)
