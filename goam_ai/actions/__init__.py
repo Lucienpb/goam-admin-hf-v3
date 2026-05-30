9dc713ee0e3dc56a580b2427ee24d908d3ba08b1
Lucien Barnes <Lucienpb@users.noreply.huggingface.co>
2026-05-30 15:30:11 +0000

    Update goam_ai/actions/__init__.py

--- goam_ai/actions/__init__.py
+++ goam_ai/actions/__init__.py
@@ -1,9 +1,15 @@
+from .summarize_player import summarize_player
+from .summarize_team import summarize_team
+from .summarize_course import summarize_course
 from .compare_players import compare_players
-from .plot_trajectory import plot_trajectory
 from .compare_trends import compare_trends
+from .plot_trajectory import plot_trajectory
 from .predict_next import predict_next
 
 __all__ = [
+    "summarize_player"
+    "summarize_team"
+    "summarize_course"
     "compare_players",
     "plot_trajectory",
     "compare_trends",
